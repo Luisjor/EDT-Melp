16ebddb109891e276a63bc83cf4e1d1296500d16
Jorge Chavez <ljorchavez@gmail.com>
2022-10-18 19:37:09 -0600

    changes

--- src/Components/Articles.js
+++ src/Components/Articles.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api';
 import {
     FacebookShareButton,
--- src/Components/credentials.js
+++
@@ -1,3 +0,0 @@
-export default {
-    mapsKey: 'AIzaSyC5QRQPWJyh8jLPz7NRS0Dyr5EUASRiO0E'
-}
